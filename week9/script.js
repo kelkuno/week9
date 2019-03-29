32cccc5ed7c4812e5979397d8a252c2a9c910e8b
Kelsey Kuno <kelsey.kuno@gmail.com>
2019-03-29 15:12:57 -0500

    final
    -js added
    -css finalized

--- week9/script.js
+++ week9/script.js
@@ -1,3 +1,19 @@
 /* ----------------------------------
 Assignment 9
 ______________________________*/
+var fact = new Vue({
+    el: '.top-ten-facts',
+    data: {
+        llamaFact1: "Llamas are smart and easy to train.",
+        llamaFact2: "In the Andes Mountains of Peru, llama fleece has been shorn and used in textiles for about 6,000 years. Llama wool is light, warm and water-repellent.",
+        llamaFact3: "Llamas are vegetarians and have efficient digestive systems.",
+        llamaFact4: "Llamas live to be about 20 years old.",
+        llamaFact5: "Llamas are social animals and prefer to live with other llamas or herd animals.",
+        llamaFact6: "Llamas don't bite. They spit when they're agitated, but that's mostly at each other.",
+        llamaFact7: "Yarn made from llama fiber is soft and lightweight, yet remarkably warm.",
+        llamaFact8: "The current population of llamas and alpacas in South America is estimated to be more than 7 million, and there are about 158,000 llamas and 100,000 alpacas in the U.S. and Canada today.",
+        llamaFact9: "Llamas come in a range of solid and spotted colors including black, gray, beige, brown, red and white.",
+        llamaFact10: "Llamas are members of the camelid, or camel, family.",
+        llamaFactConditional: true
+    }
+  })
